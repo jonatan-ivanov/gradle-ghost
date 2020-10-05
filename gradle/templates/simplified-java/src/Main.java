da37346e478c89e306c7d28dae90ad0b3ec831ae
Jonatan Ivanov <jonatan.ivanov@gmail.com>
2020-10-05 00:18:02 -0700

    Adding javadoc to the Main class

--- gradle/templates/simplified-java/src/Main.java
+++ gradle/templates/simplified-java/src/Main.java
@@ -1,7 +1,16 @@
 import lombok.extern.slf4j.Slf4j;
 
+/**
+ * Main class which holds the main method,
+ * see: <a href="https://docs.oracle.com/javase/tutorial/getStarted/application/index.html">A Closer Look at the "Hello World!" Application</a>.
+ */
 @Slf4j
 public class Main {
+    /**
+    * The entry point of the application.
+    *
+    * @param args Command-line arguments
+    */
     public static void main(String[] args) {
         log.info("Hello World");
     }
